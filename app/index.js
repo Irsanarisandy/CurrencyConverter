584804b25621dc108566a99e0dc68a0094f5c683
Irsan Arisandy <irsanarisandy@hotmail.com>
2018-05-02 23:24:21 +1200

    Add container and logo components.

--- app/index.js
+++ app/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+EStyleSheet.build({
+    $primaryBlue: '#4F6D7A',
+    $white: '#FFFFFF',
 });
 
-export default () => (
-    <View style={styles.container}>
-        <Text>Currency Converter</Text>
-    </View>
-);
+export default () => <Home />;
